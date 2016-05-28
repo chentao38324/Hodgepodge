8ae32c4794ff0aa94d1ff6cee1c9f03779d09d46
Chen Tao <1457562356@qq.com>
2016-05-28 23:50:53 +0800

    ImageLoader load web pic

--- app/src/main/java/com/ptchan/hodgepodge/MainActivity.java
+++ app/src/main/java/com/ptchan/hodgepodge/MainActivity.java
@@ -1,13 +1,35 @@
 package com.ptchan.hodgepodge;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.LruCache;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends Activity {
+
+    private BitmapCache bitmapCache;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        init();
+
+    }
+
+    private void init() {
         setContentView(R.layout.activity_main);
+        //用ImageLoader加载图片
+        loadWebPicUseImageLoader();
+    }
+
+    private void loadWebPicUseImageLoader() {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        ImageLoader imageLoader = new ImageLoader(queue,bitmapCache);
     }
 }
+
