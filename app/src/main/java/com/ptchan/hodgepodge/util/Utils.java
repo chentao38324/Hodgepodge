1f4e30451f5d99b5047fe591efed18618059d832
Chen Tao <1457562356@qq.com>
2016-07-02 22:58:30 +0800

    2016年07月02日

--- app/src/main/java/com/ptchan/hodgepodge/util/Utils.java
+++ app/src/main/java/com/ptchan/hodgepodge/util/Utils.java
@@ -4,7 +4,9 @@ package com.ptchan.hodgepodge.util;
  * Created by chentao on 16-6-30.
  */
 public class Utils {
-
+    /**
+     * 类型转换函数
+     * */
     public static void convertToInt(Object object){
 
     }
