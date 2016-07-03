eda9983735f733dc9b76fdb2eb1d17e371d2ac7a
Chen Tao <1457562356@qq.com>
2016-07-03 21:36:01 +0800

    add oom crash test

--- app/src/main/java/com/ptchan/hodgepodge/crashTest/CrashTest.java
+++ app/src/main/java/com/ptchan/hodgepodge/crashTest/CrashTest.java
@@ -2,7 +2,13 @@ package com.ptchan.hodgepodge.crashTest;
 
 /**
  * Created by chentao on 16-7-3.
+ * some crash test
  */
 public class CrashTest {
+    /**
+     * out of memory
+     */
+    public static void oom(){
 
+    }
 }
